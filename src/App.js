09995fe323fa839ff2e41059544709021724082f
AMIT KUMAR PANDEY <129199162+Amitgit1st@users.noreply.github.com>
2023-09-01 23:07:53 +0530

    App.js(Meals summary)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React,{Fragment} from "react";
 import Header from "./COMPONENT/LAYOUT/Header";
+import MealsSummary from "./COMPONENT/MEALS/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <MealsSummary/>
+      </main>
     </Fragment>
   );
 }
