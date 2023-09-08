f8f57d5fd8ce70749c3058c87bc5d8ec3761fbcf
AMIT KUMAR PANDEY <129199162+Amitgit1st@users.noreply.github.com>
2023-09-09 01:38:09 +0530

    App.js
    When user click on cart icon cart will get open and when it click on close button it gets close.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React,{Fragment} from "react";
+import React,{Fragment,useState} from "react";
 import Header from "./COMPONENT/LAYOUT/Header";
 import Meals from "./COMPONENT/MEALS/Meals";
 import Cart from "./COMPONENT/CART/Cart";
 
 function App() {
+  const [cartIsShown,setcartIsShown]=useState(false);
+
+  const showCartHandler=()=>{
+    setcartIsShown(true);
+  }
+
+  const hideCartHandler=()=>{
+    setcartIsShown(false);
+  }
+  
   return (
     <Fragment>
-      <Cart/>
-      <Header/>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals/>
       </main>
