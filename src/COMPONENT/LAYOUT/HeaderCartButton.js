cc5ff19046bf2e91f09674c2c017c1d96eff84d8
AMIT KUMAR PANDEY <129199162+Amitgit1st@users.noreply.github.com>
2023-09-10 22:10:52 +0530

    HeaderCartButton.js(context API)

--- src/COMPONENT/LAYOUT/HeaderCartButton.js
+++ src/COMPONENT/LAYOUT/HeaderCartButton.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React,{useContext} from "react";
 import classes from './HeaderCartButton.module.css';
 import CartIcon from "../CART/CartIcon";
+import CartContext from "../../STORE/cart-context";
 
 const HeaderCartButton=props=>{
+
+    const CartCtx=useContext(CartContext);
+    let Quantity=0;
+    CartCtx.items.forEach((item)=>{
+          Quantity=Quantity+ Number(item.quantity)
+    })
+
     return <button className={classes.button} onClick={props.onClick} >
         <span className={classes.icon}>
             <CartIcon/>
         </span>
         <span >Your Cart</span>
-        <span className={classes.badge}>0</span>
+        <span className={classes.badge}>{Quantity}</span>
     </button>
 
 }
