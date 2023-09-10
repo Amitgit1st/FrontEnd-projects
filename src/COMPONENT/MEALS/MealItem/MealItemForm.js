90b734cf62fe37ba338bb207464cbe50ef9b6960
AMIT KUMAR PANDEY <129199162+Amitgit1st@users.noreply.github.com>
2023-09-10 22:04:49 +0530

    MealItemForm.js(context API)

--- src/COMPONENT/MEALS/MealItem/MealItemForm.js
+++ src/COMPONENT/MEALS/MealItem/MealItemForm.js
@@ -1,17 +1,33 @@
+import { useContext } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
+import CartContext from '../../../STORE/cart-context';
+
+
 const MealItemForm = (props) => {
+
+const Cartctx=useContext(CartContext);
+
+const updateCartItems=(e)=>{
+        e.preventDefault();
+        const quantity=document.getElementById('amount' + props.id).value;
+        Cartctx.addItem({...props.item, quantity:quantity})
+       
+        
+    }
+
+
     return (
         <form className={classes.form}>
-            <Input label="Amount" input={{
-                id:'amount',
+          <Input label="Amount" input={{
+                id:'amount' + props.id,
                 type:'number',
                 min:'1',
                 max:'5',
                 step:'1',
                 defaultValue :'1',
             }} />
-            <button>+ Add</button>
+            <button onClick={updateCartItems}>+ Add</button>
         </form>
     );
 };
