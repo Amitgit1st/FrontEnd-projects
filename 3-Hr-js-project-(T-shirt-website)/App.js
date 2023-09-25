3cbd6b4c2371e048d769e8707cafeee0a9b7bbbe
AMIT KUMAR PANDEY <129199162+Amitgit1st@users.noreply.github.com>
2023-09-26 02:46:23 +0530

    App.js

--- 3-Hr-js-project-(T-shirt-website)/App.js
+++ 3-Hr-js-project-(T-shirt-website)/App.js
@@ -1,1 +1,36 @@
+import React, { useState } from "react";
+import Card from "./COMPONENT/UI/Card";
+import "./App.css";
+import EntryForm from "./COMPONENT/Form/EntryForm";
+
+import CartButton from "./COMPONENT/CART/CartButton";
+
+function App() {
+  const [TshirtData, setTshirtData] = useState([]);
+
+  const newTshirtHandler = (newData) => { setTshirtData((prevData) => [...prevData, newData]) }
+
+  return (
+    <div>
+
+      <h1 style={{ textAlign: "center" }}>Tshirt-DATA-ENTRY</h1>
+     
+      <header>
+        <CartButton />
+        <EntryForm newData={newTshirtHandler} />
+      </header>
+      <Card>
+        {TshirtData.map((Tshirt, index) => (
+          <div key={index}>
+            {Tshirt.NAME}-{Tshirt.DESCRIPTION}-{Tshirt.PRICE}-{Tshirt.QUANTITY}
+            <button>Add to cart</button>
+          </div>
+        ))}</Card>
+
+
+    </div>
+  );
+}
+
+export default App;
 
