1b760a08d5a56bfd889013ba227fcc5dc43ebf5c
AMIT KUMAR PANDEY <129199162+Amitgit1st@users.noreply.github.com>
2023-09-27 17:39:52 +0530

    Bottom.js
    Same as for building block of ecommerce

---
+++ Ecommerce Website/CartComponent/COMPONENT/LAYOUT/Bottom.js
@@ -0,0 +1,11 @@
+import { Facebook, Telegram } from 'react-bootstrap-icons';
+const Bottom = () => {
+    return (
+        <div>
+            <h1 className="text-left mb-0 bg-info text-white p-4" style={{ fontSize: "30px" }}>
+                The Generics <Facebook className="mr-2" /><Telegram className="mr-2" /></h1>
+               
+        </div>
+    )
+}
+export default Bottom;
